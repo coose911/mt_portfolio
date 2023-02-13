5d248c865070f2b4b4df8b011e207de64ef7d96a
Marcus Thomson <marcus.thomson@gmail.com>
2023-02-13 16:47:19 +0000

    added about me and skills

--- README.md
+++ README.md
@@ -68,3 +68,14 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+GIT Steps
+
+* gaa
+
+* git commit -m "message"
+
+* git Push 
+
+* npm run deploy
--- src/Container/Home.js
+++ src/Container/Home.js
@@ -75,13 +75,30 @@ const Home = () => {
                 <Paragraph1>
                     <p>
                         <h1>About Me</h1>
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        <p>"I recently left my previous career to persue my dream in software. 
+                            I am a hard working, enthusiastic and eager to learn software engineering
+                            professional with a background in mechanical engineering, sales,
+                            management and a passion for technology. I recently graduated from
+                            an intensive 16 week Professional Software Development course at
+                            CodeClan and I am looking to start a new career in a Software
+                            Engineering position."</p>
                     </p>
                 </Paragraph1>
                 <Paragraph2>
                     <p>
                         <h1>Skills</h1>
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        <p>"Over my many years in prevous careers i have learnt many transferable skills to bring into my new career as a software engineer from,
+                            - Managament 
+                            - Working in a team on large projects 
+                            - Problem Solving
+                            - Project Managment 
+                            - People Skills
+                        During my time at CodeClan on the professional software engineering course i have been able to expand and contribute new skills to aid me in my new career from,
+                            - Programming languages below
+                            - Testing - Static, Unit, E2E, using Cypress
+                            - Building projects
+                            - Working with other Developers daily and in projects
+                            - "</p>
                     </p>
                     <p>
 
