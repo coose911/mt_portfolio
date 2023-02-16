1a7546aa34aff30721360cfce289065a419295a2
Marcus Thomson <marcus.thomson@gmail.com>
2023-02-16 15:28:00 +0000

    nearly fincished projects page

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import styled from 'styled-components';
 import m from '../images/transparrent_m_logo.png'
-
+import './NavBar.css'
 
 const NavContainer = styled.div`
     background-color: #101010;
@@ -20,7 +20,6 @@ const NavContainer = styled.div`
     margin-left: -20px;
     position: fixed;
 `
-
 const LinkContainer = styled.li`
     display: block;
     text-align: center;
@@ -36,7 +35,6 @@ const LinkContainer = styled.li`
     background-color: yellow;
     } 
 `
-
 const Mlogo = styled.div`
     margin-left: -18em;
     padding: 0;
@@ -44,7 +42,6 @@ const Mlogo = styled.div`
     height: 7em;
 
 `
-
 const StyledLink = styled(Link)`
     font-size: 35px;
     color: white;
@@ -54,39 +51,6 @@ const StyledLink = styled(Link)`
         transform: scale(1.44);
     }
 `;
-
-const ContactContainer = styled.li`
-    font-size: 30px;
-    text-align: center;
-    height: 21px;
-    top: 50%;
-    margin-bottom: 100px;
-    margin-top: 100px;
-    margin-left: 0.9em;
-    width: 80%;
-    list-style-type: none;
-    text-transform: uppercase;
-    cursor: pointer;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-
-    &.actvie {
-    background-color: yellow;
-    } 
-
-    font-size: 35px;
-    color: white;
-    text-decoration: none;
-    &:hover{
-        color: yellow;
-        transform: scale(1.44);
-    }
-
-    
-`
 
 const NavBar = () => {
 
@@ -113,21 +77,22 @@ const NavBar = () => {
                 </StyledLink>
             </LinkContainer>
             <LinkContainer>
-                <StyledLink to="/contact">
+                {/* <StyledLink to="/contact">
                     <FontAwesomeIcon icon={faPhone}/>
-                </StyledLink>
+                </StyledLink> */}
+                {/* removed contact link */}
             </LinkContainer>
-            <ContactContainer>
-                <a href="https://www.linkedin.com/in/marcus-thomson-11076885/" target="_blank" rel="noreferrer">
+            <div class='contactContainer'>
+                <a class='linkedInIcon' href="https://www.linkedin.com/in/marcus-thomson-11076885/" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faLinkedinIn} color='white'/>
                 </a>
-                <a href="https://github.com/coose911?tab=repositories" target="_blank" rel="noreferrer">
+                <a class='githubIcon' href="https://github.com/coose911?tab=repositories" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faGithub} color='white'/>
                 </a>
-                <a>
+                <a class='emailIcon'>
                     <FontAwesomeIcon icon={faEnvelope} color='white'/>
                 </a>
-            </ContactContainer>
+            </div>
         </NavContainer>
     );
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,48 +2,57 @@ import { faGit, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import styled from 'styled-components';
+import './Projects.css'
+import image1 from '../Project_1_images/1.png'
+import image2 from '../Project_1_images/2.png'
+import image3 from '../Project_1_images/3.png'
+import image4 from '../Project_1_images/4.png'
+import gImage1 from '../Project_2_images/1.png'
+import gImage2 from '../Project_2_images/2.png'
+import gImage3 from '../Project_2_images/3.png'
+import gImage4 from '../Project_2_images/4.png'
 
+// const ProjectOne = styled.div`
+//     margin-left: 25px;
+//     margin-top: 5em;
+//     /* display: inline-block; */
+//     width: 90%;
+//     height: 80%;
+//     flex: 1 0 200px;
+//     background: linear-gradient(
+//     135deg,
+//     rgba(255, 255, 255, 0.1),
+//     rgba(255, 255, 255, 0.1)
+//     );
+//     backdrop-filter: blur(21px);
+//     -webkit-backdrop-filter: blur(21px);
+//     box-shadow: 0 3px 20px 0 rgba(0,0,0,0.36);
+//     border: 1px solid rgba(255,255,255,0.16);
+//     border-radius: 10px;
+//     padding: 45px;
+// `
 
-
-const ProjectOne = styled.div`
-    display: inline-block;
-    width: 40%;
-    height: 80%;
-    flex: 1 0 200px;
-    background: linear-gradient(
-    135deg,
-    rgba(255, 255, 255, 0.1),
-    rgba(255, 255, 255, 0.1)
-    );
-    backdrop-filter: blur(21px);
-    -webkit-backdrop-filter: blur(21px);
-    box-shadow: 0 3px 20px 0 rgba(0,0,0,0.36);
-    border: 1px solid rgba(255,255,255,0.16);
-    border-radius: 10px;
-    padding: 45px;
-`
-
-const ProjectTwo = styled.div`
-margin-left: 25px;
-    display: inline-block;
-    width: 40%;
-    height: 80%;
-    flex: 1 0 200px;
-    background: linear-gradient(
-    135deg,
-    rgba(255, 255, 255, 0.1),
-    rgba(255, 255, 255, 0.1)
-    );
-    backdrop-filter: blur(21px);
-    -webkit-backdrop-filter: blur(21px);
-    box-shadow: 0 3px 20px 0 rgba(0,0,0,0.36);
-    border: 1px solid rgba(255,255,255,0.16);
-    border-radius: 10px;
-    padding: 45px;
-`
+// const ProjectTwo = styled.div`
+//     margin-left: 25px;
+//     margin-top: 5em;
+//     /* display: inline-block; */
+//     width: 90%;
+//     height: 80%;
+//     flex: 1 0 200px;
+//     background: linear-gradient(
+//     135deg,
+//     rgba(255, 255, 255, 0.1),
+//     rgba(255, 255, 255, 0.1)
+//     );
+//     backdrop-filter: blur(21px);
+//     -webkit-backdrop-filter: blur(21px);
+//     box-shadow: 0 3px 20px 0 rgba(0,0,0,0.36);
+//     border: 1px solid rgba(255,255,255,0.16);
+//     border-radius: 10px;
+//     padding: 45px;
+// `
 
 const ProjectContainer = styled.div`
-    flex-wrap: flex;
     gap: 2px;
     margin-top: 10em;
     margin-left: 15em;
@@ -59,34 +68,41 @@ const Projects = () => {
         <>  
             <ProjectContainer>
                 <div>
-                    <ProjectOne>
+                    <div class='solo-project-section'>
                         <h1>Solo Project</h1>
-                        <p>To create a habit tracking app which will track the consumption of breaks, coffee and water.
-                            It will include a Diary section and a seperate page for breathing.
-                            The aim of this app is to aid busy people in taking regular breaks to avoid burn out and keep track.
+                        <h5>Python | SQL | Jinja | HTML | CSS</h5>
+                        <p>To create a Gym management system that allows the user to add, edit and delete members and classes.
+                            Allow the user to book members onto certain classes while storing all the data on a PostrSQL database using Flask.
                         </p>
-                        <video controls width='450' height='250'>
-                            <source src='../Recordings/Solo-project-recording.mov' type="video/quicktime" />
-                        </video>
-                        <a href="https://github.com/coose911/First_Solo_Project" >
+                        <section >
+                            <img  src={image4} />
+                            <img  src={image3} />
+                            <img  src={image2} />
+                            <img  src={image1} />
+                        </section>
+                        <a class='gitLink' href="https://github.com/coose911/First_Solo_Project" >
                             <FontAwesomeIcon icon ={faGithub} color='black'/>
-                            Link to Project
+                            Link to Solo Project
                         </a>
-                    </ProjectOne>
-                    <ProjectTwo>
+                    </div>
+                    <div class='group-project-section'>
                         <h1>Group Project</h1>
+                        <h5>JavaScript | React | CSS | </h5>
                         <p>To create a habit tracking app which will track the consumption of breaks, coffee and water.
                             It will include a Diary section and a seperate page for breathing.
-                            The aim of this app is to aid busy people in taking regular breaks to avoid burn out and keep track.
+                            The aim of this app is to aid the user taking regular breaks to avoid burn out and keep track of consumption storing the data in a database (MongoDB) using Express server.
                         </p>
-                        <video controls width='450' height='250'>
-                            <source src='../Recordings/Solo-project-recording.mov' type="video/quicktime" />
-                        </video>
-                        <a href="https://github.com/coose911/Breaktime_App_Project" >
+                        <section >
+                            <img  src={gImage4} />
+                            <img  src={gImage3} />
+                            <img  src={gImage2} />
+                            <img  src={gImage1} />
+                        </section>
+                        <a class='gitLink' href="https://github.com/coose911/Breaktime_App_Project" >
                             <FontAwesomeIcon icon ={faGithub} color='black'/>
-                            Link to Project
+                            Link to Group Project
                         </a>
-                    </ProjectTwo>
+                    </div>
                 </div>
             </ProjectContainer>
         </>
