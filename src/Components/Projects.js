fdbf2ed3be147e6e54523d129f1618b86c21b359
Marcus Thomson <marcus.thomson@gmail.com>
2023-02-27 21:53:02 +0000

    sorted images

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,8 @@ import image4 from '../Project_1_images/4.png'
 import gImage1 from '../Project_2_images/1.png'
 import gImage2 from '../Project_2_images/2.png'
 import gImage3 from '../Project_2_images/3.png'
-import gImage4 from '../Project_2_images/4.png'
+import gImage5 from '../Project_2_images/5.png'
+import gImage6 from '../Project_2_images/6.png'
 
 
 const ProjectContainer = styled.div`
@@ -54,7 +55,8 @@ const Projects = () => {
                             The aim of this app is to aid the user taking regular breaks to avoid burn out and keep track of consumption storing the data in a database (MongoDB) using Express server.
                         </p>
                         <section >
-                            <img  src={gImage4} />
+                            <img  src={gImage6} />
+                            <img  src={gImage5} />
                             <img  src={gImage3} />
                             <img  src={gImage2} />
                             <img  src={gImage1} />
