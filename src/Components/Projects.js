cacedf226b5811dcafcad5ead42075acc29a2f42
Marcus Thomson <marcus.thomson91@gmail.com>
2023-03-27 09:56:15 +0100

    added robit

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,6 +12,12 @@ import gImage2 from '../Project_2_images/2.png'
 import gImage3 from '../Project_2_images/3.png'
 import gImage5 from '../Project_2_images/5.png'
 import gImage6 from '../Project_2_images/6.png'
+import uImage1 from '../Robit_images/Level-1-full-map.png'
+import uImage2 from '../Robit_images/Level-3-full-map.png'
+import uImage3 from '../Robit_images/level-1-action.png'
+import uImage4 from '../Robit_images/level-3-action.png'
+import uImage5 from '../Robit_images/level-2-action.png'
+
 
 
 const ProjectContainer = styled.div`
@@ -30,6 +36,25 @@ const Projects = () => {
         <>  
             <ProjectContainer>
                 <div>
+                <div class='robit'>
+                        <h1>Robit 2D Game</h1>
+                        <h3>C# | Unity | 2 Weeks</h3>
+                        <p>To build a 2D game  using Unity and writing in C#. The game was to be a space adventure game with  player and enemies, score/collectables and as much functionality as timely possible.
+                            We also wanted to create our own characters using pixel art along with animation. We managed to design and animate the player (Robit) and the enemies.
+                            This game allows the player to operate Robit while interacting with enemies and dodge obstacles like falling rocks/spikes and swinning morning stars.
+                        </p>
+                        <section >
+                            <img  src={uImage1} />
+                            <img  src={uImage2} />
+                            <img  src={uImage3} />
+                            <img  src={uImage4} />
+                            <img  src={uImage5} />
+                        </section>
+                        <a class='gitLink' href="https://github.com/coose911/Breaktime_App_Project" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon ={faGithub} color='black'/>
+                            Link to Group Project
+                        </a>
+                    </div>
                     <div class='solo-project-section'>
                         <h1>Solo Project</h1>
                         <h3>Python | SQL | Jinja | HTML | CSS | Flask</h3>
